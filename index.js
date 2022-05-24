5c39772a690a9a7c69bf0f8d3d8b8a24d6d3d0f8
YunSukHyun <czcz9207@gmail.com>
2022-05-24 17:01:49 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('public'))
+app.get('/', (req, res) => res.send('Hello World'))
+app.get('/webapi', (req, res) => {
+    var request = require('request');
+    var options = {
+        'method': 'GET',
+        'url': 'http://apis.data.go.kr/6260000/FoodService/getFoodKr?serviceKey=fJG3TBoTlbHyOI9f0VTK9lnW2re1zlt9culWOaM9v7PLCzmSLCYDv3RcjOgf5dG3QDmj%2BuxFv560JYq8z6G4%2FA%3D%3D&numOfRows=150&pageNo=1&resultType=json',
+        'headers': {
+        }
+        
+      };
+      request(options, function (error, response) {
+        if (error) throw new Error(error);
+        // console.log(typeof(response));
+        // console.log(response);
+        
+        res.send(response.body);
+      });
+})
+app.listen(port, ()=> console.log(`Example app listening at http://localhost:${port}`))
---
+++ public/set.js
@@ -0,0 +1,113 @@
+let all = new Array();
+let clickCount = 0;
+function filterRegion(region){
+  let regionList = document.getElementById('restaurants');
+  regionList.innerHTML = "";
+  if(region==='all'){
+    for(let i = 0; i < all.length; i++){
+      if(i == 0){
+        regionList.innerHTML += "<a class='restaurant firstA'>" + all[i].MAIN_TITLE + "</a>";
+      }
+      else
+        regionList.innerHTML += "<a class='restaurant'>" + all[i].MAIN_TITLE + "</a>";
+    }
+    return;
+  }
+  const selectedRegion = all.filter(r => r.GUGUN_NM === region);
+  console.log(selectedRegion);
+
+  for(let i = 0; i < selectedRegion.length; i++){
+    const restName = selectedRegion[i].MAIN_TITLE;
+    if(i == 0){
+      regionList.innerHTML += "<a class='restaurant firstA' onclick='showRestaurantInfo(this)'>" + restName + "</a>";
+    }
+    else
+      regionList.innerHTML += "<a class='restaurant' onclick='showRestaurantInfo(this)'>" + restName + "</a>";
+  }
+  console.log(regionList.innerHTML);
+  if(regionList.innerHTML === ""){
+    regionList.innerHTML += "<a class='firstA'>맛없는 동네인듯? ㅡㅅㅡ;;</a>";
+  }
+}
+
+function showRestaurantInfo(rest){
+  let restInfo = document.getElementById('restInfo');
+  let tmp;
+  for(let i = 0; i < all.length; i++){
+    if(rest.innerHTML === all[i].MAIN_TITLE){
+      console.log(all[i]);
+      tmp = i;
+      break;
+    }
+  }
+  restInfo.innerHTML =  "";
+  restInfo.innerHTML += "<a class='firstA'>상호명: " + all[tmp].MAIN_TITLE + "</a>";
+  restInfo.innerHTML += "<a>가는길: " + all[tmp].ADDR1 + "</a>";
+  restInfo.innerHTML += "<a>연락처: " + all[tmp].CNTCT_TEL + "</a>";
+  restInfo.innerHTML += "<a class='screenshot'>대표메뉴: " + all[tmp].RPRSNTV_MENU + "</a>";
+  restInfo.innerHTML += "<a>소개: " + all[tmp].ITEMCNTNTS + "</a>";
+  kakaoMap(all[tmp].LAT, all[tmp].LNG);
+  screenshotPreview(tmp);
+}
+function displayResponse(){
+  if(clickCount > 0){
+    alert("중복 입력 방지");
+    return;
+  }
+  clickCount++;
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+  };
+  fetch("http://localhost:3000/webapi", requestOptions)
+  .then(response => response.text())
+  .then(result => {
+      let tmp = JSON.parse(result).getFoodKr.item;
+      for(let i = 0; i < tmp.length; i++){
+        all.push(tmp[i]);
+      }
+      console.log("length: ", all.length);
+  })
+  .catch(error => console.log('error', error));
+}
+
+this.screenshotPreview = function(idx){	
+	/* CONFIG */
+		xOffset = 5;
+		yOffset = 0;
+		// these 2 variable determine popup's distance from the cursor
+		// you might want to adjust to get the right result	
+	/* END CONFIG */
+	$("a.screenshot").hover(function(e){
+		this.t = this.title;
+		this.title = "";	
+		var c = (this.t != "") ? "<br/>" + this.t : "";
+		$("body").append("<p id='screenshot'><img src='"+ all[idx].MAIN_IMG_NORMAL +"' alt='url preview' />"+ c +"</p>");								 
+		$("#screenshot")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px")
+			.fadeIn("fast");						
+    },
+    function(){
+      this.title = this.t;	
+      $("#screenshot").remove();
+    });	
+	$("a.screenshot").mousemove(function(e){
+		$("#screenshot")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px");
+	});			
+};
+
+
+// starting the script on page load
+$(document).ready(function(){
+  $("h2").on({
+  mouseenter:function(){
+      $(this).css("color","green");
+  },
+  mouseleave:function(){
+      $(this).css("color","white");
+  }
+  })
+})
