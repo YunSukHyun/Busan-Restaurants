0c49d63dc469efdf5b0ec33bac10df543c3a61d0
YunSukHyun <czcz9207@gmail.com>
2023-11-27 02:24:51 +0900

    fix: remove duplicated code

--- public/map.js
+++ public/map.js
@@ -1,26 +1,15 @@
-var mapContainer = document.getElementById('map'),
-mapOption = { 
-    center: new kakao.maps.LatLng(35.23099774517053, 129.08232580812265),
-    level: 3
-};
-var map = new kakao.maps.Map(mapContainer, mapOption);
-var markerPosition  = new kakao.maps.LatLng(35.23099774517053, 129.08232580812265); 
-var marker = new kakao.maps.Marker({
-  position: markerPosition
-});
-marker.setMap(map);
-function kakaoMap(x,y){
-  var mapContainer = document.getElementById('map'),
-  mapOption = { 
+function kakaoMap(x, y) {
+  const mapContainer = document.getElementById("map"),
+    mapOption = {
       center: new kakao.maps.LatLng(x, y),
-      level: 4
-  };
-  var map = new kakao.maps.Map(mapContainer, mapOption);
-  var markerPosition  = new kakao.maps.LatLng(x, y); 
-  var marker = new kakao.maps.Marker({
-    position: markerPosition
+      level: 4,
+    };
+  const map = new kakao.maps.Map(mapContainer, mapOption);
+  const markerPosition = new kakao.maps.LatLng(x, y);
+  const marker = new kakao.maps.Marker({
+    position: markerPosition,
   });
   marker.setMap(map);
 }
 
-
+kakaoMap(35.23099774517053, 129.08232580812265);
