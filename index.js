26c9352a2d7e044e0eab27c643d4e175b205ca61
YunSukHyun <czcz9207@gmail.com>
2023-11-27 16:10:59 +0900

    package: remove depreacted

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
 app.use(express.static("public"));
-app.get("/", (req, res) => res.send("Hello World"));
-app.get("/webapi", (req, res) => {
-  var request = require("request");
+app.get("/webapi", async (req, res) => {
   const restNum = req.query.restNum;
-  var options = {
-    method: "GET",
-    url:
-      "http://apis.data.go.kr/6260000/FoodService/getFoodKr?serviceKey=fJG3TBoTlbHyOI9f0VTK9lnW2re1zlt9culWOaM9v7PLCzmSLCYDv3RcjOgf5dG3QDmj%2BuxFv560JYq8z6G4%2FA%3D%3D&numOfRows=" +
+  const good = await fetch(
+    "http://apis.data.go.kr/6260000/FoodService/getFoodKr?serviceKey=fJG3TBoTlbHyOI9f0VTK9lnW2re1zlt9culWOaM9v7PLCzmSLCYDv3RcjOgf5dG3QDmj%2BuxFv560JYq8z6G4%2FA%3D%3D&numOfRows=" +
       encodeURI(restNum) +
-      "&pageNo=1&resultType=json",
-    headers: {},
-  };
-  request(options, function (error, response) {
-    if (error) throw new Error(error);
-    res.send(response.body);
-  });
+      "&pageNo=1&resultType=json"
+  );
+  const data = await good.json();
+  if (!data) throw new Error("Fail to fetch data");
+  res.send(data);
 });
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 );
